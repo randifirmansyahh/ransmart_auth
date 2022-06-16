e48188861d5e3e6175bde87903d9325a9d884943
Randi Firmansyah <randykelvin26@gmail.com>
2022-06-16 17:11:14 +0700

    fix db

--- app/server/server.go
+++ app/server/server.go
@@ -6,5 +6,7 @@ func getConnectionString() string {
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
-	return "root:@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	return dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 }
